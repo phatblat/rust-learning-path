e221c82f7531a6369dc4ba8f56fd91602f2a760e
Ben Chatelain <ben@octop.ad>
2022-10-09 12:43:10 -0600

    🏷 Print boolean

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -34,4 +34,8 @@ fn main() {
 
     // Integer and Floating point division
     println!("9 / 2 = {} but 9.0 / 2.0 = {}", 9u32 / 2, 9.0 / 2.0);
+
+    // Declare variable to store result of "greater than" test, Is 1 > 4? -- false
+    let is_bigger = 1 > 4;
+    println!("Is 1 > 4? {}", is_bigger);
 }
