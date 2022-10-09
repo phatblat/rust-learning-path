6b2d7c1372176388dc08f43a545ef3f369c8f9df
Ben Chatelain <ben@octop.ad>
2022-10-09 12:36:10 -0600

    👤 Shadow varaible

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -14,4 +14,15 @@ fn main() {
     // Change the value of an immutable variable
     a_number = 15;
     println!("Now the number is {}.", a_number);
+
+    // Declare first variable binding with name "shadow_num"
+    let shadow_num = 5;
+
+    // Declare second variable binding, shadows existing variable "shadow_num"
+    let shadow_num = shadow_num + 5;
+
+    // Declare third variable binding, shadows second binding of variable "shadow_num"
+    let shadow_num = shadow_num * 2;
+
+    println!("The shadowed number is {}.", shadow_num);
 }
